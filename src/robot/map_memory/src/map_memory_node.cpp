d8c347aca1ed532e44eeb65c1a20ee4d3b694d4b
MikeKatsnelson <mike.katsnelson@gmail.com>
2025-01-18 01:23:45 -0500

    fixes to memory map

--- src/robot/map_memory/src/map_memory_node.cpp
+++ src/robot/map_memory/src/map_memory_node.cpp
@@ -1,3 +1,7 @@
+#include  <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <geometry_msgs/msg/quaternion.h>
+
 #include "map_memory_node.hpp"
 
 MapMemoryNode::MapMemoryNode() : 
@@ -21,60 +25,107 @@ MapMemoryNode::MapMemoryNode() :
             std::chrono::seconds(1), std::bind(&MapMemoryNode::updateMap, this));
 }
 
+/*
+ * Get a "local costmap" based on lidar scan
+ */
 void MapMemoryNode::costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
-  // store latest costmap
+  // store as latest costmap
   latest_costmap_ = *msg;
   costmap_updated_ = true;
 }
 
+/*
+ * Get "odometry" / positioning of robot
+ */
 void MapMemoryNode::odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
+  // robot's current coordinates
   double x = msg->pose.pose.position.x;
   double y = msg->pose.pose.position.y;
 
   // compute distance traveled from last map update
   double distance = std::sqrt(std::pow(x - last_x, 2) + std::pow(y - last_y, 2));
   
-  // set new robot position
+  // set new robot position if robot traveled great enough distance
   if (distance >= distance_threshold) {
       last_x = x;
       last_y = y;
       should_update_map_ = true;
+
+      latest_odom_ = *msg;
+
       RCLCPP_INFO(this->get_logger(), "distance threshold passed");
   }
 }
 
+/*
+ * Check every second whether global costmap should be updated
+ * Update global costmap if conditions are met
+ */
 void MapMemoryNode::updateMap() {
-  // global_map_.info.width = 300;
-  // global_map_.info.height = 300;
-  // map_pub_->publish(global_map_);
-  // return;
-  
   if (should_update_map_ && costmap_updated_) {
+    // integrate latest "local" costmap into global costmap
     integrateCostmap();
+    
+    // publish
     map_pub_->publish(global_map_);
-    RCLCPP_INFO(this->get_logger(), "Publishing: New global map");
     should_update_map_ = false;
+
+    RCLCPP_INFO(this->get_logger(), "Publishing: New global map");
   }
 }
 
-// integrate the latest costmap into the global map
+  /*
+   *  Integrate latest "local" costmap into global costmap
+   */
   void MapMemoryNode::integrateCostmap() {
-    // if function is called for the 1st time
-    if (global_map_.info.width == 0 && global_map_.info.height == 0) {
-      // copy latest costmap into global costmap
-        // takes care of grid alignment (ensures both are same size)
-      global_map_ = latest_costmap_;
-      RCLCPP_INFO(this->get_logger(), "init globa_map");
-      return;
-    }
-    
-    // transform and merge latest costmap into global map
-    for (int i = 0; i < latest_costmap_.info.width * latest_costmap_.info.height; i++) {
-      if (latest_costmap_.data[i] != -1) {
-        // cell is known = update global map
-        global_map_.data[i] = latest_costmap_.data[i];
+    // robot's current position and orientation
+    int robot_x = latest_odom_.pose.pose.position.x;
+    int robot_y = latest_odom_.pose.pose.position.y;
+    double robot_yaw = quaternionToYaw(latest_odom_.pose.pose.orientation);
+
+    for (int j = 0; j < latest_costmap_.info.height; j++) {
+      for (int i = 0; i < latest_costmap_.info.width; i++) {
+        // index in local costmap
+        int local_index = j * latest_costmap_.info.width + i;
+
+        // cell is unknown -> do not update global map
+        if (latest_costmap_.data[local_index] == -1)
+          continue;
+
+        // "local" coordinates of cell
+        int local_x = latest_costmap_.info.origin.position.x + i;
+        int local_y = latest_costmap_.info.origin.position.y + j;
+
+        // get the "global" coordinates of the cell
+          // i.e where it actually is on the global map
+        int global_x = local_x * std::cos(robot_yaw) - local_y * std::sin(robot_yaw) + robot_x;
+        int global_y = local_x * std::sin(robot_yaw) - local_y * std::cos(robot_yaw) + robot_y;
+        
+        // translate global cooridnates into "cells" in costmap
+        int global_i = global_x - global_map_.info.origin.position.x;
+        int global_j = global_y - global_map_.info.origin.position.y;
+
+        // skip if index is out of bounds
+        if ((global_i < 0 || global_i >= global_map_.info.width) || (global_j < 0 || global_j >= global_map_.info.height)) 
+          continue;
+
+        // index in global costmap
+        int global_index = global_j * global_map_.info.width + global_i;
+
+        // overwrite old value in global costmap with new one
+        global_map_.data[global_index] = latest_costmap_.data[local_index];
       }
     }
+  }
+
+  /*
+   * Helper function
+   */
+  double MapMemoryNode::quaternionToYaw(const geometry_msgs::msg::Quaternion &q) {
+    tf2::Quaternion quat(q.x, q.y, q.z, q.w);
+    tf2::Matrix3x3 mat(quat);
+    double roll, pitch, yaw;
+    mat.getRPY(roll, pitch, yaw);
   }
 
 int main(int argc, char ** argv)
